d86437147977b5f3393b28851bdb57ff2dc32e11
Satish Gowda <satishgowda@Satishs-MacBook-Pro-2.local>
2026-03-05 17:07:09 +0530

    open router clone

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+export const prisma = new PrismaClient({
+  adapter,
+});
